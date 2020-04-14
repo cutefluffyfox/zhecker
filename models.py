82c95d7281765d2c084e85da3c29c3a738c69653
cutefluffyfox <mishkazel1@gmail.com>
2020-04-15 00:01:49 +0300

    Test and Task method added User changed All errors changed to assert

--- models.py
+++ models.py
@@ -15,6 +15,63 @@ class Test(SqlAlchemyBase):
     output = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     points = sqlalchemy.Column(sqlalchemy.Integer, default=1)
 
+    def change_data(self, inp=None, out=None, points=None):
+        """
+        Change input/output/points, return
+        {'status': 'ok'}
+        {'status': 'invalid <input/output/points> type, expected <str/int> or None'}
+        {'status': 'same test has already been added'}
+        """
+        try:
+            session = create_session()
+            assert inp is None or type(inp) == str, 'invalid input type, expected str'
+            assert out is None or type(out) == str, 'invalid output type, expected str'
+            assert points is None or type(points) == int, 'invalid input type, expected int'
+            assert len(session.query(Test).filter(Test.task_id == self.task_id, Test.input == (self.input if inp is None else inp), Test.output == (self.output if out is None else out)).all()) == 0, 'same test has already been added'
+            test = session.query(Test).filter(Test.id == self.id).first()
+            if inp is not None:
+                self.input = test.input = inp
+            if out is not None:
+                self.output = test.output = out
+            if points is not None:
+                self.points = test.points = points
+            session.commit()
+            return {'status': 'ok'}
+        except AssertionError as ex:
+            return {'status': ex.args[0]}
+
+    @staticmethod
+    def get_test(test_id: int):
+        """Return Test(**kwargs) by test_id or None if id is invalid"""
+        session = create_session()
+        return session.query(Test).filter(Test.id == test_id).first()
+
+    @staticmethod
+    def add_test(task_id: int, inp: str, out: str, points=None) -> dict:
+        """
+        Add test to database, return:
+        {'status': 'ok', 'id': int}
+        {'status': 'invalid <input/output/points> type, expected <str/int>'}
+        {'status': 'no task with id '<task_id>''}
+        {'status': 'same test has already been added'}
+        """
+        try:
+            session = create_session()
+            assert len(session.query(Task).filter(Task.id == task_id).all()) == 1, f'no task with id \'{task_id}\''
+            assert type(inp) == str, 'invalid input type, expected str'
+            assert type(out) == str, 'invalid output type, expected str'
+            assert points is None or type(points) == int, 'invalid points type, expected int'
+            assert len(session.query(Test).filter(Test.task_id == task_id, Test.input == inp, Test.output == out).all()) == 0, 'same test has already been added'
+            test = Test(task_id=task_id,
+                        input=inp,
+                        output=out,
+                        points=points)
+            session.add(test)
+            session.commit()
+            return {'status': 'ok', 'id': session.query(Test).filter(Test.task_id == task_id, Test.input == inp, Test.output == out).first().id}
+        except AssertionError as ex:
+            return {'status': ex.args[0]}
+
 
 class Task(SqlAlchemyBase):
     __tablename__ = 'tasks'
@@ -26,9 +83,95 @@ class Task(SqlAlchemyBase):
     reference = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     attempts = sqlalchemy.Column(sqlalchemy.Integer, default=0)
     successful = sqlalchemy.Column(sqlalchemy.Integer, default=0)
-    """Нужен метод add_task(title, description ?input, output?)"""
-    """Нужен метод  Task.get_task_by_title(title) """
-    """add_task"""
+
+    def change_data(self, title=None, description=None, reference=None):
+        """
+        Change title/description/reference, return
+        {'status': 'ok'}
+        {'status': 'same task has already been added'}
+        {'status': 'invalid <title/description/reference> type, expected str or None'}
+        """
+        try:
+            session = create_session()
+            assert title is None or type(title) == str, 'invalid title type, expected str or None'
+            assert description is None or type(description) == str, 'invalid description type, expected str or None'
+            assert reference is None or type(reference) == str, 'invalid reference type, expected str or None'
+            assert len(session.query(Task).filter(Task.creator == self.creator, Task.title == (self.title if title is None else title), Task.description == (self.description if description is None else description), Task.reference == (self.reference if reference is None else reference)).all()) == 0, "same task has already been added"
+            task = session.query(Task).filter(Task.id == self.id).first()
+            if title is not None:
+                self.title = task.title = title
+            if description is not None:
+                self.description = task.description = description
+            if reference is not None:
+                self.reference = task.reference = reference
+            session.commit()
+            return {'status': 'ok'}
+        except AssertionError as ex:
+            return {'status': ex.args[0]}
+
+    def get_tests(self):
+        """Return [Test(**kwargs), ...] sorted by index (recent added at the back)"""
+        session = create_session()
+        return session.query(Test).filter(Test.task_id == self.id).order_by(sqlalchemy.desc(~Test.id)).all()
+
+    @staticmethod
+    def get_all(sort_type='easy'):
+        """Return [Task(**kwargs), ...] sorted by sort_type (see models.py -> class Task() -> method get_all())"""
+        sort = {
+            'new': Task.id,
+            'old': ~Task.id,
+            'easy': Task.successful / Task.attempts,
+            'hard': ~(Task.successful / Task.attempts),
+            'popular': Task.attempts,
+            'not_popular': ~Task.attempts,
+            'most_success': Task.successful,
+            'least_success': ~Task.successful,
+        }
+        session = create_session()
+        return session.query(Task).order_by(sqlalchemy.desc(sort[sort_type])).all()
+
+    @staticmethod
+    def get_task(task_id: int):
+        """Return Task(**kwargs) by task_id or None if id is invalid"""
+        session = create_session()
+        return session.query(Task).filter(Task.id == task_id).first()
+
+    @staticmethod
+    def get_task_by_title(title: str):
+        """Return [Task(**kwargs), ...] by title where Task.title like title"""
+        session = create_session()
+        return session.query(Task).filter(Task.title.like(Task.__like(title))).all()
+
+    @staticmethod
+    def __like(text: str) -> str:
+        return '%' + text + '%'
+
+    @staticmethod
+    def add_task(creator: int, title: str, description: str, reference: str) -> dict:
+        """
+        Add task to database, return:
+        {'status': 'ok', 'id': int}
+        {'status': 'invalid <title/description/reference> type, expected str'}
+        {'status': 'no user with id '<creator>''}
+        {'status': 'same task has already been added'}
+        """
+        try:
+            session = create_session()
+            user = session.query(User).filter(User.id == creator).first()
+            assert user is not None, f"no user with id '{creator}'"
+            assert type(title) == str, 'invalid title type, expected str'
+            assert type(description) == str, 'invalid description type, expected str'
+            assert type(reference) == str, 'invalid reference type, expected str'
+            assert len(session.query(Task).filter(Task.creator == creator, Task.title == title, Task.description == description, Task.reference == reference).all()) == 0, "same task has already been added"
+            task = Task(creator=creator,
+                        title=title,
+                        description=description,
+                        reference=reference)
+            session.add(task)
+            session.commit()
+            return {'status': 'ok', 'id': session.query(Task).filter(Task.creator == creator, Task.title == title, Task.description == description, Task.reference == reference).first().id}
+        except AssertionError as ex:
+            return {'status': ex.args[0]}
 
 
 class Contest(SqlAlchemyBase):
@@ -45,6 +188,7 @@ class Contest(SqlAlchemyBase):
 
     """get_contest_by_title"""
 
+
 class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
 
@@ -59,59 +203,43 @@ class User(SqlAlchemyBase, UserMixin):
     verification = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     contests = sqlalchemy.Column(sqlalchemy.String, default='')
 
-    def change_unique(self, username=None, email=None) -> dict:
+    def change_data(self, username=None, email=None, name=None, surname=None, city=None, password=None) -> dict:
         """
-        Change username or email, return
+        Change username/email/name/surname/city/password, return
         {'status': 'ok'}
-        {'status': 'UNIQUE constraint failed: users.<username/email>'}
-        {'status': 'invalid <username/email> type, expected str or None'}
+        {'status': '<username/email> is already taken'}
+        {'status': 'invalid <username/email/name/surname/city/password> type, expected str or None'}
         """
         try:
-            assert type(username) == str or username is None, 'invalid username type, expected str or None'
-            assert type(email) == str or email is None, 'invalid email type, expected str or None'
             session = create_session()
+            assert username is None or type(username) == str, 'invalid username type, expected str or None'
+            assert email is None or type(email) == str, 'invalid email type, expected str or None'
+            assert name is None or type(name) == str, 'invalid name type, expected str or None'
+            assert surname is None or type(surname) == str, 'invalid surname type, expected str or None'
+            assert city is None or type(city) == str, 'invalid city type, expected str or None'
+            assert password is None or type(password) == str, 'invalid password type, expected str or None'
+            assert len(session.query(User).filter(User.username == username).all()) == 0 or username is None, 'username is already taken'
+            assert len(session.query(User).filter(User.email == email).all()) == 0 or email is None, 'email is already taken'
             user = session.query(User).filter(User.id == self.id).first()
             if username is not None:
                 user.username = self.username = username
             if email is not None:
                 user.email = self.email = email
+            if name is not None:
+                user.name = self.name = name
+            if surname is not None:
+                user.surname = self.surname = surname
+            if city is not None:
+                user.city = self.city = city
+            if password is not None:
+                user.password = self.password = self.__generate_password(password)
             session.commit()
             return {'status': 'ok'}
-        except sqlalchemy.exc.IntegrityError as ex:
-            return {'status': ex.args[0][25:]}
         except AssertionError as ex:
             return {'status': ex.args[0]}
 
-    def change_password(self, password: str) -> dict:
-        """
-        Change password, return
-        {'status': 'ok'}
-        {'status': 'invalid password type, expected str'}
-        """
-        try:
-            session = create_session()
-            hashed_password = self.__generate_password(password)
-            user = session.query(User).filter(User.id == self.id).first()
-            user.password = self.password = hashed_password
-            session.commit()
-            return {'status': 'ok'}
-        except ValueError:
-            return {'status': 'invalid password type, expected str'}
-
-    def change_personal_data(self, name=None, surname=None, city=None):
-        """Change name, surname, city"""
-        session = create_session()
-        user = session.query(User).filter(User.id == self.id).first()
-        if name is not None:
-            user.name = self.name = name
-        if surname is not None:
-            user.surname = self.surname = surname
-        if city is not None:
-            user.city = self.city = city
-        session.commit()
-
     def __repr__(self):
-        return f"User(username={self.username}, email={self.email}>"
+        return f"User(username={self.username}, email={self.email})"
 
     @staticmethod
     def get_user(user_id: int):
@@ -126,7 +254,7 @@ class User(SqlAlchemyBase, UserMixin):
         return session.query(User).filter(User.username == username, User.password == User.__generate_password(password)).first()
 
     @staticmethod
-    def __generate_password(password: str):
+    def __generate_password(password: str) -> str:
         return sha3_256((password + load(open('config.json', 'r', encoding='UTF-8'))['SALT']).encode('UTF-8')).hexdigest()
 
     @staticmethod
@@ -134,12 +262,16 @@ class User(SqlAlchemyBase, UserMixin):
         """
         Add user to database, return:
         {'status': 'ok', 'id': int}
-        {'status': 'invalid password type, expected str'}
-        {'status': 'UNIQUE constraint failed: users.<username or email>'}
-        {'status': 'NOT NULL constraint failed: users<NOT NULL column>'}
+        {'status': 'invalid <password/name/surname> type, expected str'}
+        {'status': '<username/email> is already taken'}
         """
         try:
             session = create_session()
+            assert type(password) == str, 'invalid password type, expected str'
+            assert type(name) == str, 'invalid name type, expected str'
+            assert type(surname) == str, 'invalid surname type, expected str'
+            assert len(session.query(User).filter(User.username == username).all()) == 0, 'username is already taken'
+            assert len(session.query(User).filter(User.email == email).all()) == 0, 'email is already taken'
             session.add(User(username=username,
                              email=email,
                              password=User.__generate_password(password),
@@ -149,12 +281,8 @@ class User(SqlAlchemyBase, UserMixin):
                              verification="".join([chr(randint(65, 90)) if randint(0, 1) else chr(randint(97, 122)) for _ in range(30)])))
             session.commit()
             return {'status': 'ok', 'id': session.query(User).filter(User.username == username).first().id}
-        except ValueError:
-            return {'status': 'invalid password type, expected str'}
-        except TypeError as ex:
+        except AssertionError as ex:
             return {'status': ex.args[0]}
-        except sqlalchemy.exc.IntegrityError as ex:
-            return {'status': ex.args[0][25:]}
 
 
 class Attempt(SqlAlchemyBase):
